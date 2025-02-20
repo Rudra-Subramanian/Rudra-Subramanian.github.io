fa2a7d9c31e5246ce945c37e15090eaa3b9d04f0
NYSBC-Rudra <133771854+NYSBC-Rudra@users.noreply.github.com>
2025-02-20 14:49:30 -0500

    adding js to test, changing image size

--- strats_pages/valorant_map_loader.js
+++ strats_pages/valorant_map_loader.js
@@ -11,3 +11,38 @@ Array.from(document.getElementsByClassName("menu-item"))
 menuItems.onmouseleave = () => {
     menu.setAttribute('data-active-index', '11');
 };
+
+const maps = document.getElementById("maps");
+
+const positions = [
+  '80vh', '64vh', '48vh', '32vh', '16vh', '0vh',
+  '-16vh', '-32vh', '-48vh', '-64vh', '-80vh', '0vh'
+];
+
+
+
+// Array.from(document.getElementsByClassName("menu-item"))
+// .forEach((item, index) => {
+//   item.onmouseover = () => {
+//     menu.setAttribute('data-active-index', index);
+//     maps.style.top = positions[index];
+//   };
+// });
+
+// Array.from(document.getElementsByClassName("menu-item"))
+//   .forEach((item, index) => {
+//     item.onmouseover = () => {
+//       menu.setAttribute('data-active-index', index);
+//       maps.style.top = positions[index];
+//       Array.from(maps.children).forEach((map, mapIndex) => {
+//         if (mapIndex === index) {
+//           Object.assign(map.style, mapStyles);
+//         } else {
+//           map.style.width = '30vh';
+//           map.style.position = 'relative';
+//           map.style.marginLeft = '-45vw';
+//           map.style.marginRight = '-45vw';
+//         }
+//       });
+//     };
+//   });
