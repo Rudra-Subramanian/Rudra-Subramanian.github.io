73fad027890cd7401cf4a52d7aed3a2c22e74a0a
Rudra-Subramanian <Rudra.subramanian@gmail.com>
2025-02-21 15:23:54 -0500

    better valorant page

--- strats_pages/valorant_map_loader.js
+++ strats_pages/valorant_map_loader.js
@@ -1,33 +1,39 @@
 const menu = document.getElementById("menu");
 const menuItems = document.getElementById("menu-items");
+let map_items = document.getElementById("maps");
+map_items = map_items.children; 
+
 
 Array.from(document.getElementsByClassName("menu-item"))
   .forEach((item, index) => {
     item.onmouseover = () => {
       menu.setAttribute('data-active-index', index);
+      maps.style.top = (80 - (index*16)).toString() + 'vh';
+      // Object.assign(map_items[index].style, {
+      //   width: '50vh',
+      //   position: 'relative',
+      //   marginLeft: '-45vw',
+      //   marginRight: '-45vw',
+      // })
     };
+  //   menuItems.onmouseleave = () => {
+  //     maps.style.top = '0vh';
+  //     Object.assign(map_items[index].style, {
+  //       width: maps.style.width,
+  //       position: maps.style.position,
+  //       MarginLeft: maps,
+  //       MarginRight: 'inherit',
+  //     })
+
+  // };
+
+
   });
 
-menuItems.onmouseleave = () => {
-    menu.setAttribute('data-active-index', '11');
-};
+
 
 const maps = document.getElementById("maps");
 
-const positions = [
-  '80vh', '64vh', '48vh', '32vh', '16vh', '0vh',
-  '-16vh', '-32vh', '-48vh', '-64vh', '-80vh', '0vh'
-];
-
-
-
-// Array.from(document.getElementsByClassName("menu-item"))
-// .forEach((item, index) => {
-//   item.onmouseover = () => {
-//     menu.setAttribute('data-active-index', index);
-//     maps.style.top = positions[index];
-//   };
-// });
 
 // Array.from(document.getElementsByClassName("menu-item"))
 //   .forEach((item, index) => {
