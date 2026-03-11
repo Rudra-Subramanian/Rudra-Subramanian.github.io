d99a86ad5f6b575abedd555bf7c354907c4c4015
Rudra-Subramanian <Rudra.subramanian@gmail.com>
2026-03-10 21:36:22 -0400

    Adding getting sacked video to project index videos
    Also add guides in project index on how properly add new buttons with videos and a tag for sending it to someone with the video loaded

--- Scripts/Project_Videos.js
+++ Scripts/Project_Videos.js
@@ -1,12 +1,18 @@
 const VideosVideoControl = document.getElementById("VideosVideoControl");
 
+// To load specific video use ?video=tag in URL
 const video_load = new URLSearchParams(document.location.search).get('video');
 
 console.log(video_load);
 
 if (video_load){
+    //To add a ?video=tag add another if statement
     if (video_load.toLowerCase() === "newdavidbowie"){
         VideosVideoControl.src = '../videos/new_david_bowie_visualiser.mp4';
+        VideosVideoControl.classList.remove("is-hidden");
+    }
+    if (video_load.toLowerCase() === "sack"){
+        VideosVideoControl.src = '../videos/howiimagineasack.mp4';
         VideosVideoControl.classList.remove("is-hidden");
     }
     
